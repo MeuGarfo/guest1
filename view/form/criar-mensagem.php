d94a5a9e4b4991b8fd1eaa7b010b04740ed7152f
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-06-04 16:33:39 -0300

    M  view/form/criar-mensagem.php

--- view/form/criar-mensagem.php
+++ view/form/criar-mensagem.php
@@ -2,12 +2,14 @@
 $action=$_ENV['SITE_URL'].'mensagem';
 ?>
 <form class="form-vertical" action="<?php print $action; ?>" method="post" id="mensagem">
-<fieldset>
-    <legend>Criar mensagem</legend>
-    <label for="mensagem-body">Mensagem</label>
-    <input class="input-block-level" type="text" name="body" value="" id="mensagem-body" maxlength="60" required>
-    <button type="submit" name="button">Enviar mensagem</button>
-</fieldset>
+    <fieldset>
+        <legend>Criar mensagem</legend>
+        <label for="mensagem-body">Mensagem</label>
+        <input class="input-block-level" type="text" name="body" value="" id="mensagem-body" maxlength="60" required>
+        <div class="text-center">
+            <button type="submit" name="button">Enviar mensagem</button>
+        </div>
+    </fieldset>
 </form>
 <script type="text/javascript">
 document.getElementById('mensagem-body').onkeydown = function(e){
